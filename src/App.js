9d083f3b0b1dd591492b92a39098387ec180db10
Luis Salazar <luissa@lagash.com>
2020-01-22 13:24:30 -0300

    React Project base files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const App = () => {
+  return (
+    <div>
+      <h1>React Project Base</h1>
+    </div>
+  )
+}
+
+App.displayName = 'App'
+
+export default App
